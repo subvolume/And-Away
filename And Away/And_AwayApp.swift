11a5e5403e193185b91c42053873e3d94f0a1311
Oliver Lukman <oliver@subvolume.com>
2025-05-29 14:15:23 +0200

    Initial Commit

---
+++ And Away/And_AwayApp.swift
@@ -0,0 +1,17 @@
+//
+//  And_AwayApp.swift
+//  And Away
+//
+//  Created by Oliver Lukman on 29/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct And_AwayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
