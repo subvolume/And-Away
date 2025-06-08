d00d0653e1f18956aa18f483925801d0004863db
Oliver Lukman <oliver@subvolume.com>
2025-06-08 16:20:32 +0200

    added actions to PlaceDetailsActions

--- And Away/Views/Components/PlaceDetailsActions.swift
+++ And Away/Views/Components/PlaceDetailsActions.swift
@@ -2,11 +2,24 @@ import SwiftUI
 
 struct PlaceDetailsActions: View {
     @State private var isBookmarked = false
+    @State private var isTodaySelected = false
     
     var body: some View {
         HStack {
             PillButton(icon: "bookmark.fill", isSelected: isBookmarked) {
                 isBookmarked.toggle()
+            }
+            
+            PillButton(text: isTodaySelected ? "Today!" : "Today?", icon: isTodaySelected ? "checkmark" : "plus", isSelected: isTodaySelected) {
+                isTodaySelected.toggle()
+            }
+            
+            PillButton(icon: "message.fill") {
+                // Chat action
+            }
+            
+            PillButton(icon: "ellipsis") {
+                // Menu action
             }
             
             Spacer()
